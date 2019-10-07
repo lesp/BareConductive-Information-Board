69a1b6ea0e27a6b34350e625e0fb8664a99406a8
Les Pounder <lespounder@gmail.com>
2019-10-07 14:11:46 +0100

    Removed merge issue

--- Python Code on Pi/receiver.py
+++ Python Code on Pi/receiver.py
@@ -52,19 +52,3 @@ while True:
         print("No Bare Conductive Touch Board found")
         logger.error("No Bare Conductive Touch Board has been found.")
         break
-    with serial.Serial('/dev/ttyACM0', 9600, timeout=1) as ser:
-        line = str(ser.readline())
-        line = line[2]
-        print(line)
-        if line == "0":
-            play_video("/home/pi/Pathe/0.mp4")
-        elif line == "1":
-            play_video("/home/pi/Pathe/1.mp4")
-        elif line == "2":
-            play_video("/home/pi/Pathe/2.mp4")
-        elif line == "3":
-            play_video("/home/pi/Pathe/3.mp4")
-        elif line == "4":
-            play_video("/home/pi/Pathe/Mini-Mega.mp4")
-        elif line == "5":
-            play_video("/home/pi/Pathe/Mini-Mega.mp4")
